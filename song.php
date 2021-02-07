a8e741fc49e3170c9d0396ec6edfc8548862c4e9
CcDalao <842105949@qq.com>
2021-02-07 09:01:34 +0800

    V2.0 更新

--- song.php
+++ song.php
@@ -1,14 +1,43 @@
 <?php
+
 /*
 CcDalao
 获取网易云音乐中歌曲中的热评信息
+2021年2月6日
+NetEase-cloud-music-review-API 2.0
 */
+
 if ($_GET['id']) {
-    $musicid = $_GET['id'];
-    $offset = $_GET['offset'];
+    $music_outchain_id = $_GET['id'];
+    $Get_music_outchain_contents = file_get_contents('https://music.163.com/api/v2/playlist/detail?id='.$music_outchain_id, false, $context);
+    $Get_music_outchain_contents_decode = json_decode($Get_music_outchain_contents, true);
+    $id = 0;
+    foreach ($Get_music_outchain_contents_decode['playlist']['trackIds'] as $musicid) {
+        $id++;
+        $musicidarr = array($musicid['id']);
+        $musicidarrs[] = $musicidarr;
+
+    }
+
+    function randid($json,$musicids) {
+        $musicids = $musicids-1;
+        $bb = mt_rand(0,$musicids);
+        return($json[$bb][0]);
+
+
+    }
+    $musicidarrs_json = $musicidarrs;
+    
+    //以上新增功能 从歌单中随机获取id 好久没写代码了。技术变菜了
+    // $music_get_id
+    
+    $music_get_id = randid($musicidarrs,$id);
+    
+    //下为原代码
+     $offset = $_GET['offset'];
     $time = 0;
     $arr = 0;
-    $jsonStr = file_get_contents('https://music.163.com/api/v1/resource/comments/R_SO_4_' . $musicid . '?limit=15&offset=' . $offset . '', false, $context);
+    $jsonStr = file_get_contents('https://music.163.com/api/v1/resource/comments/R_SO_4_' . $music_get_id . '?limit=15&offset=' . $offset . '', false, $context);
     $jsonStrs = json_decode($jsonStr, true);
     foreach ($jsonStrs['hotComments'] as $val) {
         $time++;
@@ -25,4 +54,6 @@ if ($_GET['id']) {
     $aaaa = array_slice($arrrs, -2, 1);
     $arrb = array_merge_recursive($arrr, $aaaa);
     exit(json_encode($arrb));
+    
+    
 }
